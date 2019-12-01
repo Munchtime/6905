2f12d10885b0fb53c4d5a1dc9cba4a7283e6479f
karesia <58379968+karesia@users.noreply.github.com>
2019-12-01 19:15:35 -0400

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+2#!/bin/bash
 
 aws cloudformation create-stack \
-    --template-body file://CloudFormationEC2InstanceSG.yml \
-    --stack-name  CloudFormationEC2InstanceSG \
+    --template-body file://group2ec2stack.yml \
+    --stack-name  group2ec2stack \
     --region us-west-2 \
     --parameters ParameterKey=KeyName,ParameterValue=group2.pem \
       ParameterKey=InstanceType,ParameterValue=t2.micro
